11a2e171bfa69131e3cc3e8c9af59668cd5b7e86
Abraham Kuri <kurenn@icalialabs.com>
2015-06-18 20:53:20 -0500

    refactors links controller spec

--- spec/controllers/api/v1/links_controller_spec.rb
+++ spec/controllers/api/v1/links_controller_spec.rb
@@ -11,8 +11,7 @@ RSpec.describe Api::V1::LinksController, :type => :controller do
       end
 
       it "returns 4 records" do
-        links_response = json_response
-        expect(links_response[:links]).to have(4).items
+        expect(json_response[:links]).to have(4).items
       end
 
       it "responds with a 200 code" do
@@ -35,8 +34,7 @@ RSpec.describe Api::V1::LinksController, :type => :controller do
       end
 
       it "returns 3 records" do
-        links_response = json_response
-        expect(links_response[:links]).to have(3).items
+        expect(json_response[:links]).to have(3).items
       end
     end
 
@@ -50,8 +48,7 @@ RSpec.describe Api::V1::LinksController, :type => :controller do
     end
 
     it "returns a single record" do
-      link_response = json_response
-      expect(link_response[:link][:id]).to eq(@link.id)
+      expect(json_response[:link][:id]).to eq(@link.id)
     end
 
   end
@@ -64,8 +61,7 @@ RSpec.describe Api::V1::LinksController, :type => :controller do
 
     it "updates the link 'title' to 'The new iPhone is out!'" do
       put :update, { id: @link.id, link: {title: "The new iPhone is out!"} }
-      link_response = json_response
-      expect(link_response[:link][:title]).to eq "The new iPhone is out!"
+      expect(json_response[:link][:title]).to eq "The new iPhone is out!"
     end
 
     context 'when record does not update' do
@@ -74,8 +70,7 @@ RSpec.describe Api::V1::LinksController, :type => :controller do
       end
 
       it "with a blank title" do
-        link_response = json_response
-        expect(link_response[:link][:errors][:title]).to include I18n.t('errors.messages.blank')
+        expect(json_response[:link][:errors][:title]).to include I18n.t('errors.messages.blank')
       end
 
       it { should respond_with 422 }
@@ -94,18 +89,13 @@ RSpec.describe Api::V1::LinksController, :type => :controller do
   describe "POST #create" do
 
     it "renders the link record if success" do
-      #post :create, link: FactoryGirl.attributes_for(:link)
       post :create, { link: { title: "Look mom I'm hacking", url: "http://imawesome.com" } }
-      link_response = json_response
-      #expect(link_response[:link]).to have_key(:title)
-      expect(link_response[:link][:title]).to eq "Look mom I'm hacking"
+      expect(json_response[:link][:title]).to eq "Look mom I'm hacking"
     end
 
     it "renders errors if link not saved" do
       post :create, { link: { title: "", url: "http://imawesome.com" } }
-      link_response = json_response
-      #expect(link_response[:link]).to have_key(:title)
-      expect(link_response[:link][:errors][:title]).to include I18n.t('errors.messages.blank')
+      expect(json_response[:link][:errors][:title]).to include I18n.t('errors.messages.blank')
     end
   end
 end
