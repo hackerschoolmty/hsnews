d35879e0338a31953d137f96e2d12f05cd60f047
Abraham Kuri <kurenn@icalialabs.com>
2015-06-22 21:03:00 -0500

    Adds auth token generation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     uniqueness: { :case_sensitive => false },
                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  
+  validates :auth_token, uniqueness: true
+
+  before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = SecureRandom.base64(64)
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#generate_authentication_token!" do
+    before(:each) do
+      @user = FactoryGirl.build :user
+    end
+
+    it "generates a unique token" do
+      allow(SecureRandom).to receive(:base64).and_return("auniquetoken")
+      expect{ @user.generate_authentication_token! }.to change{@user.auth_token}.from(nil).to("auniquetoken")
+    end
+
+    it "avoids nil on the auth token" do
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to_not be_nil
+    end
+
+  end
 end
