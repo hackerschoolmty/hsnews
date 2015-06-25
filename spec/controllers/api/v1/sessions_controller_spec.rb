8396ad05b7681c3e5bf15c44d8cbee7e3ee93238
Abraham Kuri <kurenn@icalialabs.com>
2015-06-24 19:07:01 -0500

    Adds sessions create action specs

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::SessionsController, :type => :controller do
+  let(:user) { FactoryGirl.create :user }
+
+  describe "POST #create" do
+
+    it "renders the user when credentials are correct" do
+      post :create, session: { email: user.email, password: "12345678" }
+      expect(json_response[:user]).to have_key(:auth_token)
+    end
+
+    it "renders an error message whne credentials are invalid" do
+      post :create, session: { email: user.email, password: "qwerty" }
+      expect(json_response[:session]).to have_key(:errors)
+    end
+
+    it "renders an error message when no email is sent" do
+      post :create, session: { password: "qwerty" }
+      expect(json_response[:session]).to have_key(:errors)
+    end
+  end
 
 end
